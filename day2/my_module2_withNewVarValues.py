4e7e8134e84b69cb6406c29c83c7bff82bba89f9
Denis Vrdoljak <denisvrdoljak@users.noreply.github.com>
2020-09-02 06:59:15 -0700

    Day 2, second half

---
+++ day2/my_module2_withNewVarValues.py
@@ -0,0 +1,47 @@
+# empty .py file
+
+generic_setting1 = 22
+generic_setting2 = 'abcd'
+
+def count_letters_numbers4(input_string):
+    """input: input_string: any string of letters or numbers
+    output: string containing the counts of the numbers and letters in input_string"""
+
+    letters = 0
+    digits = 0
+    valid_letters = list('abcdefghijklmnopqrstuvwxyz')
+    valid_digits = list('0123456789')
+
+
+    try:
+        for letter in input_string.lower():
+            if letter in valid_letters:
+                letters += 1
+            elif letter in valid_digits:
+                digits += 1
+        return_string = f'Letters: {letters}\n Digits: {digits}'
+    except:
+        return_string = "error, bad input (from try/except block)"
+
+    return return_string
+
+
+def count_letters_numbers(input_string):
+    """input: input_string: any string of letters or numbers
+    output: string containing the counts of the numbers and letters in input_string"""
+
+
+    letters = 0
+    digits = 0
+    valid_letters = list('abcdefghijklmnopqrstuvwxyz')
+    valid_digits = list('0123456789')
+
+
+    for letter in input_string.lower():
+        if letter in valid_letters:
+            letters += 1
+        elif letter in valid_digits:
+            digits += 1
+    return f'Letters: {letters}\n Digits: {digits}'
+
+
